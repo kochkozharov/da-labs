b30a5f20fa56f95197017c83748c1fc91aa28ae6
Ivan Kochkozharov <tegusigaalpa@gmail.com>
2024-03-09 01:15:59 +0300

    ???!!!

--- lab1/src/benchmark.cpp
+++ lab1/src/benchmark.cpp
@@ -1,12 +1,4 @@
-#include <algorithm>
-#include <chrono>
-#include <iostream>
-#include <memory>
-#include <random>
-#include <vector>
-
-#include "sort.h"
-
+#include "header.h"
 
 int main() {
     TVector<TKeyValuePair> data;
@@ -16,31 +8,29 @@ int main() {
     std::mt19937 gen(rd());
     std::uniform_real_distribution<double> distr(-100, 100);
 
-    std::size_t numberOfElements = 1000000;
+    const std::size_t numberOfElements = 20000000;
+
     for (size_t i = 0; i < numberOfElements; ++i) {
-        auto pair = TKeyValuePair(distr(gen), "test");
-        data.PushBack(pair);
-        benchmarkData.push_back(pair);
+        data.PushBack(TKeyValuePair(distr(gen), "test"));
+        benchmarkData.emplace_back(distr(gen), "test");
+
     }
-    auto start1 = std::chrono::high_resolution_clock::now();
+    // Benchmarking bucket sort
+    auto start = std::chrono::high_resolution_clock::now();
     BucketSort(data);
-    auto finish1 = std::chrono::high_resolution_clock::now();
+    auto end = std::chrono::high_resolution_clock::now();
+    std::cout << "Bucket sort time: " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << " microseconds" << std::endl;
 
-    auto cmp = [](const TKeyValuePair& a, const TKeyValuePair& b) {
-        return a.Key() < b.Key();
-    };
 
-    auto start3 = std::chrono::high_resolution_clock::now();
-    std::stable_sort(benchmarkData.begin(), benchmarkData.end(), cmp);
-    auto finish3 = std::chrono::high_resolution_clock::now();
 
-    auto duration1 =
-        std::chrono::duration_cast<std::chrono::microseconds>(finish1 - start1);
-    auto duration3 =
-        std::chrono::duration_cast<std::chrono::microseconds>(finish3 - start3);
-    std::cout << "my sort duration: " << duration1.count() << std::endl;
-    std::cout << "std stable_sort duration: " << duration3.count() << std::endl;
+
+    // Benchmarking std::sort
+    start = std::chrono::high_resolution_clock::now();
+    std::stable_sort(benchmarkData.begin(), benchmarkData.end());
+    end = std::chrono::high_resolution_clock::now();
+    std::cout << "std::stable_sort time: " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << " microseconds" << std::endl;
+
+
 
     return 0;
 }
-

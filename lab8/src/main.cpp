f30da3536a7a10f163a0d9f9ca1ebd2226f5ed19
Ivan Kochkozharov <tegusigaalpa@gmail.com>
2024-11-04 20:45:12 +0300

    refactor lab8

--- lab8/src/main.cpp
+++ lab8/src/main.cpp
@@ -1,67 +1,69 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <algorithm>
-// #include <chrono>
 
-const int MAX_NUM = 50;
-int m, n;
+constexpr const int MAX_NUM = 50;
 
-int FindRow(std::vector<std::vector<double>>& v, int t) {
+struct Addition {
+    Addition(size_t n) : ratios(n) {}
+    std::vector<double> ratios;
+    int price;
+    int index;
+};
+
+int FindLowestPriceRow(std::vector<Addition> &v, int t) {
+    size_t m = v.size();
+    size_t n = v[0].ratios.size();
     int minPrice = MAX_NUM + 1;
     int index = -1;
     for (int i = t; i < m; ++i) {
-        if ((v[i][t] != 0.0) && (v[i][n] < minPrice)) {
+        if ((v[i].ratios[t] != 0.0) && (v[i].price < minPrice)) {
             index = i;
-            minPrice = v[i][n];
+            minPrice = v[i].price;
         }
     }
     return index;
 }
 
-void SubtractRows(std::vector<std::vector<double>>& v, int t) {
+void SubtractRows(std::vector<Addition> &v, int t) {
+    size_t m = v.size();
+    size_t n = v[0].ratios.size();
     for (int i = t + 1; i < m; ++i) {
-        double coeff = v[i][t] / v[t][t];
+        double coeff = v[i].ratios[t] / v[t].ratios[t];
         for (int j = t; j < n; ++j) {
-            v[i][j] -= v[t][j] * coeff;
+            v[i].ratios[j] -= v[t].ratios[j] * coeff;
         }
     }
 }
 
 int main() {
-    // std::chrono::steady_clock::time_point start = 
-    //     std::chrono::steady_clock::now();
+    int n, m;
     std::cin >> m >> n;
     std::vector<int> res;
-    std::vector<std::vector<double>> additions
-                            (m, std::vector<double> (n + 2));
+    std::vector<Addition> additions(m, Addition(n));
     for (int i = 0; i < m; ++i) {
-        for (int j = 0; j < n + 1; ++j) {
-            std::cin >> additions[i][j];
+        for (int j = 0; j < n; ++j) {
+            std::cin >> additions[i].ratios[j];
         }
-        additions[i][n + 1] = i;
+        std::cin >> additions[i].price;
+        additions[i].index = i;
     }
-
     for (int i = 0; i < n; ++i) {
-        int index = FindRow(additions, i);
+        int index = FindLowestPriceRow(additions, i);
         if (index == -1) {
             std::cout << "-1" << std::endl;
             return 0;
         }
 
         std::swap(additions[i], additions[index]);
-        res.push_back(additions[i][n + 1]);
+        res.push_back(additions[i].index);
         SubtractRows(additions, i);
     }
 
     std::sort(res.begin(), res.end());
-    for (int i = 0; i < res.size(); ++i) {
-        std::cout << res[i] + 1;
-        if (i == res.size() - 1) {
-            std::cout << std::endl;
-        } else {
-            std::cout << " ";
-        }
+    for (auto r : res) {
+        std::cout << r + 1 << ' ';
     }
-
+    std::cout << '\n';
     return 0;
 }
